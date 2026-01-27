96a6c4af95bbde86acc8728ba192d4feaed4e109
hideosuzuki2024fx-blip <hideosuzuki2024fx@gmail.com>
2026-01-27 21:05:04 +0900

    v2/map: render native hex map instead of redirecting to v1

--- app/v2/map/page.tsx
+++ app/v2/map/page.tsx
@@ -1,8 +1,7 @@
-import { redirect } from "next/navigation";
+import HexMapClient from "./HexMapClient";
 
 export const dynamic = "force-dynamic";
 
 export default function V2MapPage() {
-  // Temporary: v2 front-door to v1 map until v2-native map is ready.
-  redirect("/v1/map");
+  return <HexMapClient />;
 }
